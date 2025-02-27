aeea9facfb7a5fd492d0b1f24d4fa9b1f4bfb0d8
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-02-27 14:36:40 -0300

    feat: add login view with styles and responsive design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,35 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+@extends('layouts.app')
+
+@section('title', 'Login')
+
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css//views/auth/account.css') }}">
+    <link rel="stylesheet" href="{{ asset('css//views/auth/login.css') }}">
+@endpush
+
+@section('content')
+    <div class="loginContainer">
+        <div class="imageContainer">
+            <img src="{{ asset('images/login.svg') }}" alt="Login">
+        </div>
+        <form class="loginForm">
+            <h1>Bem vindo de volta!</h1>
+            <div class="form-group">
+                <label class ="label" for="email"><i class="fi fi-rs-envelope"></i>E-mail</label>
+                <input type="email" id="email" name="email" class="form-control" required placeholder="Digite seu e-mail">
+            </div>
+
+            <div class="form-group">
+                <label class ="label" for="password"><i class="fi fi-rs-lock"></i>Senha</label>
+                <input type="password" id="password" name="password" class="form-control" required placeholder="Digite sua senha">
+            </div>
+
+            <div class="form-group">
+                <input type="submit" value="Entrar" class="btn submitBtn">
+            </div>
+            <a href="#" class="accountLink">Não tem uma conta? Cadastre-se</a>
+        </form>
+        
+    </div>
+@endsection
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,23 @@
     <title>
         @yield('title')
     </title>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-straight/css/uicons-regular-straight.css'>
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @stack('styles')
     @stack('scripts')
 </head>
 <body>
-    @stack('content')
+    <x-header />
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        <p>Feito por <span>Jabas</span> com <span>♥</span></p>
+        <div>
+            <a href="https://www.flaticon.com/uicons">Uicons by Flaticon</a>
+            <a href="https://storyset.com/online">Online illustrations by Storyset</a>
+        </div>
+    </footer>
 </body>
 </html>

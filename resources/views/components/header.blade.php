635f30d19e76e6f23d1904c34a45fadf904c76b8
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-02-27 17:03:35 -0300

    feat: add header component with logo and updated styles

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,9 @@
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/components/header.css') }}">
+@endpush
+
+<header>
+    <div class="logo">
+        <img src="{{ asset('images/logo.png') }}" alt="Logo">
+    </div>
+</header>

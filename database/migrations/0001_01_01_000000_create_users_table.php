1f173d7c42dd058103932c4f6acabeaec77786c5
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-03-02 15:17:41 -0300

    feat: update users table migration to include cpfcnpj, phone, and verification fields

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('cpfcnpj')->unique();
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('verification_code', 6)->nullable(); 
+            $table->timestamp('verification_code_expires_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();

4880d7da30aa78dfc40b6f5c464ff2e912ac81e4
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-04-11 14:35:55 -0300

    feat: enhance graph options with improved styling and descriptions

--- resources/views/graph/index.blade.php
+++ resources/views/graph/index.blade.php
@@ -14,15 +14,27 @@
 
         <div class="graphs-options">
             <a class="graph-option" href="{{ route('graph.income-x-expense') }}">
-                <h3>Receita X Despesa</h3>
+                <h3 class="graph-option-title">
+                    <i class="fi fi-rs-stats"></i>
+                    Receita X Despesa
+                </h3>
+                <span class="graph-option-description">Veja o comparativo</span>
             </a>
 
             <a class="graph-option" href="{{ route('graph.income-evolution') }}">
-                <h3>Receita ao longo dos meses</h3>
+                <h3 class="graph-option-title income">
+                    <i class="fi fi-rs-chart-line-up"></i>
+                    Receita ao longo dos meses
+                </h3>
+                <span class="graph-option-description">Evolução mês a mês</span>
             </a>
 
             <a class="graph-option" href="{{ route('graph.expense-evolution') }}">
-                <h3>Despesa ao longo dos meses</h3>
+                <h3 class="graph-option-title expense">
+                    <i class="fi fi-rs-chart-line-up"></i>
+                    Despesa ao longo dos meses
+                </h3>
+                <span class="graph-option-description">Evolução mês a mês</span>
             </a>
             <!-- <div class="graph-option">
                 <h3>Evolução da Receita</h3>
